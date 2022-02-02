b60c58842cb57442e7a419bea2d2ad2a0d987333
abdullah-beg <98782131+abdullah-beg@users.noreply.github.com>
2022-02-02 21:44:30 +0000

    Changed GameBoard Class Layout
    GameBoard instead uses an ArrayList of Piles

--- src/Solitaire/GameBoard.java
+++ src/Solitaire/GameBoard.java
@@ -1,39 +1,26 @@
 package Solitaire;
 
+import java.util.ArrayList;
+
 public class GameBoard {
     
-    private Pile p1, p2, p3, p4, p5, p6, p7;
+    private ArrayList<Pile> piles;
 
     public GameBoard() {
 
-        p1 = new Pile();
-        p2 = new Pile();
-        p3 = new Pile();
-        p4 = new Pile();
-        p5 = new Pile();
-        p6 = new Pile();
-        p7 = new Pile();
+        piles = new ArrayList<>();
 
-    }
+        for(int i = 0; i < 7; i++) {
+            piles.add(new Pile());
 
-    public Pile getPile(int pile) {
-
-        switch (pile) {
-
-            case 0: return p1;
-            case 1: return p2;
-            case 2: return p3;
-            case 3: return p4;
-            case 4: return p5;
-            case 5: return p6;
-            case 6: return p7;
         }
 
-        return null;
-
     }
 
+    public Pile getPile(int index) {
 
+        return piles.get(index);
 
+    }
 
 }
