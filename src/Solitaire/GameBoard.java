d65bee5c6a7b970868872d9029eac0912b2635b6
abdullah-beg <98782131+abdullah-beg@users.noreply.github.com>
2022-02-04 04:06:25 +0000

    Moved most of Main into GameBoard
    Made more sense to have a lot of those methods inside of GameBoard instead of Main.
    
    When a GameBoard object is initialised, it is essentially creating the entire board rather than having it being split into two classes.

--- src/Solitaire/GameBoard.java
+++ src/Solitaire/GameBoard.java
@@ -1,21 +1,73 @@
 package Solitaire;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class GameBoard {
     
+    private ArrayList<Card> cards;
     private ArrayList<Pile> piles;
     private Stock stock;
     private Waste waste;
 
     public GameBoard() {
 
+        cards = new ArrayList<>();
         piles = new ArrayList<>();
-        stock = new Stock();
         waste = new Waste();
+        stock = new Stock();
 
         for(int i = 0; i < 7; i++) {
             piles.add(new Pile());
+
+        }
+
+        generateCards();
+        Collections.shuffle(cards);
+        prepareCardLayout();
+        showFrontCard();
+        stock.setStock(cards);
+
+    }
+
+    private void generateCards() {
+
+        String suits = "ABCD";
+        String colour = "black";
+
+        for (int suit = 0; suit < 4; suit++) {
+            for (int card = 1; card < 14; card++) {
+                if (suit >= 2) {
+                    colour = "red";
+                    
+                }
+
+                cards.add(new Card(suits.substring(suit, suit + 1), card, colour));
+
+            }
+
+        }
+
+    }
+
+    private void prepareCardLayout() {
+
+        for (int pile = 0; pile < 7; pile++) {
+            for (int card = 0; card <= pile; card++) {
+                getPile(pile).addCardToPile(cards.get(card));
+                cards.remove(card);
+
+            }
+
+        }
+
+    }
+
+    private void showFrontCard() {
+
+        for (int pile = 0; pile < 7; pile++) {
+            Pile currentPile = getPile(pile);
+            currentPile.getCardAtIndex(currentPile.getCardCount() - 1).setVisible(true);
 
         }
 
--- src/Solitaire/Main.java
+++ src/Solitaire/Main.java
@@ -1,71 +1,14 @@
 package Solitaire;
 
-import java.util.ArrayList;
-import java.util.Collections;
-
 public class Main {
 
-    private ArrayList<Card> cards;
     private GameBoard game;
     private Parser parser;
 
     private Main() {
 
         parser = new Parser();
-
-        cards = new ArrayList<Card>();
-        generateCards();
         game = new GameBoard();
-
-        Collections.shuffle(cards); // List of cards is shuffled
-        prepareCardLayout(); // Place randomised cards in piles, such that you have 1 2 3 4 5 6 7 cards
-        game.getStock().setStock(cards); // Remaining cards put into stock
-        showFrontCard(); // Shows the bottom most card in each pile
-
-    }
-
-    /* 
-        This method generates all the cards. Numbered from 1 to 13, and with suit A, B, C or D
-    */
-    private void generateCards() {
-
-        String suits = "ABCD";
-        String colour = "black";
-
-        for (int suit = 0; suit < 4; suit++) {
-            for (int card = 1; card < 14; card++) {
-                if (suit == 2 || suit == 3) {
-                    colour = "red";
-                    
-                }
-                cards.add(new Card(suits.substring(suit, suit + 1), card, colour));
-
-            }
-
-        }
-
-    }
-
-    private void prepareCardLayout() {
-
-        for (int pile = 0; pile < 7; pile++) {
-            for (int card = 0; card <= pile; card++) {
-                game.getPile(pile).addCardToPile(cards.get(card));
-                cards.remove(card);
-
-            }
-
-        }
-
-    }
-
-    private void showFrontCard() {
-
-        for (int pile = 0; pile < 7; pile++) {
-            Pile currentPile = game.getPile(pile);
-            currentPile.getCardAtIndex(currentPile.getCardCount() - 1).setVisible(true);
-
-        }
 
     }
 
@@ -98,6 +41,9 @@ public class Main {
 
                 System.out.println("valid");
 
+            } else {
+
+                
             }
 
 
