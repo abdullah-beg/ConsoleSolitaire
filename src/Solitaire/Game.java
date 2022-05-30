8ef2c09868894f41256c869f2a1a4ba2e9d27968
abdullah-beg <98782131+abdullah-beg@users.noreply.github.com>
2022-05-31 00:02:11 +0100

    Added restart command functionality

---
+++ src/Solitaire/Game.java
@@ -0,0 +1,251 @@
+package Solitaire;
+
+import java.util.ArrayList;
+
+public class Game {
+    
+
+        // game map 87 x 41
+        private GameBoard game;
+        private Parser parser;
+        private Logic logic;
+        private boolean validMove;
+    
+        public Game() {
+    
+            parser = new Parser();
+            game = new GameBoard();
+            logic = new Logic();
+            validMove = false;
+    
+        }
+
+        public GameBoard getBoard() {
+
+            return game;
+
+        }
+
+        public Parser getParser() {
+
+            return parser;
+
+        }
+
+        public Logic getLogic() {
+
+            return logic;
+
+        }
+    
+        public boolean gameFinished() {
+    
+            for (Foundation foundation : game.getFoundations()) {
+                if (foundation.isEmpty() || foundation.getBottomCard().getCardNumber() != 13) {
+                    return false;
+    
+                }
+    
+            }
+    
+            return true;
+    
+        }
+    
+        public void processCommand(String word1) {
+    
+            if (word1.equals("help")) {
+                System.out.println("get good");
+                return;
+    
+            } else if (word1.equals("undo")) {
+                System.out.println("idk how to undo yet");
+                return;
+    
+            } else if (word1.equals("restart")) {
+                System.out.println("You have selected restart!");
+                restartGame();
+                return;
+    
+            }
+    
+            // If this has been reached, they have inputted "s"
+    
+            logic.cycleStock(game.getStock(), game.getWaste());
+    
+        }
+    
+        public void processCommand(String word1, String word2) {
+    
+    
+            if (word1.equals("w")) {
+    
+                Waste waste = game.getWaste();
+    
+                if (word2.substring(0,1).equals("p")) {
+                    
+                    Pile pile = game.getPile(Integer.parseInt((word2).substring(1,2)) - 1);
+    
+                    if (logic.moveLogic(waste, pile)) {
+                        
+                        pile.addCardToPile(waste.getFrontCard());
+                        waste.removeCardFromBottom();
+                        waste.setCardOrder();
+    
+                        setValidMove(true);
+            
+                    }
+    
+    
+                } else {
+                    
+                    Foundation foundation = game.getFoundation(Integer.parseInt((word2).substring(1,2)) - 1);
+    
+                    if (logic.moveLogic(waste, foundation)) {
+    
+                        foundation.addCardToPile(waste.getFrontCard());
+                        waste.removeCardFromBottom();
+                        waste.setCardOrder();
+    
+                        setValidMove(true);
+    
+                    }
+    
+                }
+    
+            } else if (word1.substring(0,1).equals("f")) { 
+    
+                Foundation foundation = game.getFoundation(Integer.parseInt((word1).substring(1,2)) - 1);
+    
+                if (word2.substring(0,1).equals("p")) {
+    
+                    Pile pile = game.getPile(Integer.parseInt((word2).substring(1,2)) - 1);
+    
+                    if (logic.moveLogic(foundation, pile)) {
+    
+                        pile.addCardToPile(foundation.getBottomCard());
+                        foundation.removeCardFromBottom();
+    
+                        setValidMove(true);
+            
+                    }
+    
+    
+                } else {
+    
+                    Foundation foundation2 = game.getFoundation(Integer.parseInt((word2).substring(1,2)) - 1);
+    
+                    if (logic.moveLogic(foundation, foundation2)) {
+    
+                        foundation2.addCardToPile(foundation.getBottomCard());
+                        foundation.removeCardFromBottom();
+    
+                        setValidMove(true);
+            
+                    }
+    
+    
+                }
+    
+            } else {
+    
+                Pile pile = game.getPile(Integer.parseInt((word1).substring(1,2)) - 1);
+    
+                if (word2.substring(0,1).equals("p")) {
+    
+                    Pile pile2 = game.getPile(Integer.parseInt((word2).substring(1,2)) - 1);
+    
+                    if (logic.moveLogic(pile, pile2)) {
+                        for (Card card : pile.getVisibleCards()) {
+                            pile2.addCardToPile(card);
+                            pile.getCardsInPile().remove(card);
+                        }
+    
+                        setValidMove(true);
+            
+                    }
+    
+                } else {
+    
+                    Foundation foundation = game.getFoundation(Integer.parseInt((word2).substring(1,2)) - 1);
+    
+                    if (logic.moveLogic(pile, foundation)) {
+                    
+                        foundation.addCardToPile(pile.getBottomCard());
+                        pile.removeCardFromBottom();
+    
+                        setValidMove(true);
+            
+                    }
+    
+                }
+    
+            }
+    
+        }
+    
+        public void processCommand(String word1, String word2, String word3) {
+    
+            Pile pile1 = game.getPile(Integer.parseInt((word1).substring(1,2)) - 1);
+            Pile pile2 = game.getPile(Integer.parseInt((word3).substring(1,2)) - 1);
+            int cardNumber = Integer.parseInt((convertInput(word2)));
+            
+            if (logic.moveLogic(pile1, cardNumber, pile2) != null) {
+                ArrayList<Card> addCards = logic.moveLogic(pile1, cardNumber, pile2);
+                
+                for (Card card : addCards) {
+                    // Add each of the new cards to Pile2
+                    pile2.addCardToPile(card);
+    
+                    // Remove each card from Pile1
+                    pile1.getCardsInPile().remove(card);
+    
+                }
+    
+                setValidMove(true);
+                
+            }
+    
+    
+        }
+    
+        private String convertInput(String input) {
+    
+            if (input.equals("a")) {
+                return "1";
+    
+            } else if (input.equals("j")) {
+                return "11";
+    
+            } else if (input.equals("q")) {
+                return "12";
+    
+            } else if (input.equals("k")) {
+                return "13";
+    
+            }
+    
+            return input;
+    
+        }
+    
+        public void setValidMove(boolean input) {
+    
+            validMove = input;
+    
+        }
+    
+        public boolean getValidMove() {
+    
+            return validMove;
+    
+        }
+
+        private void restartGame() {
+
+            System.out.println("Restarting game...");
+            game = new GameBoard();
+
+        }
+
+}
--- src/Solitaire/Main.java
+++ src/Solitaire/Main.java
@@ -1,230 +1,19 @@
 package Solitaire;
 
-import java.util.ArrayList;
-
 public class Main {
-
-    // game map 87 x 41
-    private GameBoard game;
-    private Parser parser;
-    private Logic logic;
-    private boolean validMove;
-
-    private Main() {
-
-        parser = new Parser();
-        game = new GameBoard();
-        logic = new Logic();
-        validMove = false;
-
-    }
-
-    private boolean gameFinished() {
-
-        for (Foundation foundation : game.getFoundations()) {
-            if (foundation.isEmpty() || foundation.getBottomCard().getCardNumber() != 13) {
-                return false;
-
-            }
-
-        }
-
-        return true;
-
-    }
-
-    private void processCommand(String word1) {
-
-        if (word1.equals("help")) {
-            System.out.println("get good");
-            return;
-
-        } else if (word1.equals("undo")) {
-            System.out.println("idk how to undo yet");
-            return;
-
-        }
-
-        // If this has been reached, they have inputted "s"
-
-        logic.cycleStock(game.getStock(), game.getWaste());
-
-    }
-
-    private void processCommand(String word1, String word2) {
-
-
-        if (word1.equals("w")) {
-
-            Waste waste = game.getWaste();
-
-            if (word2.substring(0,1).equals("p")) {
-                
-                Pile pile = game.getPile(Integer.parseInt((word2).substring(1,2)) - 1);
-
-                if (logic.moveLogic(waste, pile)) {
-                    
-                    pile.addCardToPile(waste.getFrontCard());
-                    waste.removeCardFromBottom();
-                    waste.setCardOrder();
-
-                    setValidMove(true);
-        
-                }
-
-
-            } else {
-                
-                Foundation foundation = game.getFoundation(Integer.parseInt((word2).substring(1,2)) - 1);
-
-                if (logic.moveLogic(waste, foundation)) {
-
-                    foundation.addCardToPile(waste.getFrontCard());
-                    waste.removeCardFromBottom();
-                    waste.setCardOrder();
-
-                    setValidMove(true);
-
-                }
-
-            }
-
-        } else if (word1.substring(0,1).equals("f")) { 
-
-            Foundation foundation = game.getFoundation(Integer.parseInt((word1).substring(1,2)) - 1);
-
-            if (word2.substring(0,1).equals("p")) {
-
-                Pile pile = game.getPile(Integer.parseInt((word2).substring(1,2)) - 1);
-
-                if (logic.moveLogic(foundation, pile)) {
-
-                    pile.addCardToPile(foundation.getBottomCard());
-                    foundation.removeCardFromBottom();
-
-                    setValidMove(true);
-        
-                }
-
-
-            } else {
-
-                Foundation foundation2 = game.getFoundation(Integer.parseInt((word2).substring(1,2)) - 1);
-
-                if (logic.moveLogic(foundation, foundation2)) {
-
-                    foundation2.addCardToPile(foundation.getBottomCard());
-                    foundation.removeCardFromBottom();
-
-                    setValidMove(true);
-        
-                }
-
-
-            }
-
-        } else {
-
-            Pile pile = game.getPile(Integer.parseInt((word1).substring(1,2)) - 1);
-
-            if (word2.substring(0,1).equals("p")) {
-
-                Pile pile2 = game.getPile(Integer.parseInt((word2).substring(1,2)) - 1);
-
-                if (logic.moveLogic(pile, pile2)) {
-                    for (Card card : pile.getVisibleCards()) {
-                        pile2.addCardToPile(card);
-                        pile.getCardsInPile().remove(card);
-                    }
-
-                    setValidMove(true);
-        
-                }
-
-            } else {
-
-                Foundation foundation = game.getFoundation(Integer.parseInt((word2).substring(1,2)) - 1);
-
-                if (logic.moveLogic(pile, foundation)) {
-                
-                    foundation.addCardToPile(pile.getBottomCard());
-                    pile.removeCardFromBottom();
-
-                    setValidMove(true);
-        
-                }
-
-            }
-
-        }
-
-    }
-
-    private void processCommand(String word1, String word2, String word3) {
-
-        Pile pile1 = game.getPile(Integer.parseInt((word1).substring(1,2)) - 1);
-        Pile pile2 = game.getPile(Integer.parseInt((word3).substring(1,2)) - 1);
-        int cardNumber = Integer.parseInt((convertInput(word2)));
-        
-        if (logic.moveLogic(pile1, cardNumber, pile2) != null) {
-            ArrayList<Card> addCards = logic.moveLogic(pile1, cardNumber, pile2);
-            
-            for (Card card : addCards) {
-                // Add each of the new cards to Pile2
-                pile2.addCardToPile(card);
-
-                // Remove each card from Pile1
-                pile1.getCardsInPile().remove(card);
-
-            }
-
-            setValidMove(true);
-            
-        }
-
-
-    }
-
-    private String convertInput(String input) {
-
-        if (input.equals("a")) {
-            return "1";
-
-        } else if (input.equals("j")) {
-            return "11";
-
-        } else if (input.equals("q")) {
-            return "12";
-
-        } else if (input.equals("k")) {
-            return "13";
-
-        }
-
-        return input;
-
-    }
-
-    private void setValidMove(boolean input) {
-
-        validMove = input;
-
-    }
-
-    private boolean getValidMove() {
-
-        return validMove;
-
-    }
 
     public static void main(String[] args) {
 
-        Main main = new Main();
+        Game game = new Game();
         Draw draw = new Draw();
 
-        while (!main.gameFinished()) {
+        game.getBoard().showFrontCard();
+        draw.printBoard(game.getBoard().getStock().getCardCount(), game.getBoard().getWaste(), game.getBoard().getFoundations(), game.getBoard().getPiles(), game.getValidMove());
+        game.setValidMove(false);
 
-            Command command = main.parser.getUserCommand();
+        while (!game.gameFinished()) {
+
+            Command command = game.getParser().getUserCommand();
 
             if (command.validateCommand()) {
 
@@ -233,23 +22,26 @@ public class Main {
                 String word3 = command.getThirdWord();
 
                 if (command.getSecondWord() == null) {
-                    main.processCommand(word1);
+                    game.processCommand(word1);
 
                 } else if (command.getThirdWord() == null) {
-                    main.processCommand(word1, word2);
+                    game.processCommand(word1, word2);
 
                 } else {
-                    main.processCommand(word1, word2, word3);
+                    game.processCommand(word1, word2, word3);
 
                 }
 
             }
 
-            main.game.showFrontCard();
-            draw.printBoard(main.game.getStock().getCardCount(), main.game.getWaste(), main.game.getFoundations(), main.game.getPiles(), main.getValidMove());
-            main.setValidMove(false);
+            game.getBoard().showFrontCard();
+            draw.printBoard(game.getBoard().getStock().getCardCount(), game.getBoard().getWaste(), game.getBoard().getFoundations(), game.getBoard().getPiles(), game.getValidMove());
+            game.setValidMove(false);
 
         }
+
+        System.out.println("!!!~~~~~~~~~~GAME OVER~~~~~~~~~~!!!");
+        System.out.println("!!!~~~~~~~~~~~YOU WON~~~~~~~~~~~!!!");
 
     }
 
